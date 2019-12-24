915477abc32b224e2a26682849bfda7cd2baee75
John Pennypacker <jpennypacker@uri.edu>
2019-12-24 11:26:23 -0500

    updates js styles in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,138 +1,129 @@
-var gulp = require( 'gulp' );
-var pkg = require( './package.json' );
+const gulp = require( "gulp" );
+const pkg = require( "./package.json" );
 
-var banner = ['/*',
-  'Theme Name: <%= pkg.themeName %>',
-  'Theme URI: <%= pkg.homepage %>',
-  'Author: <%= pkg.author %>',
-  'Author URI: <%= pkg.authorURI %>',
-  'Description: <%= pkg.description %>',
-  'Template: <%= pkg.template %>',
-  'Version: <%= pkg.version %>',
-  'License: <%= pkg.license %>',
-  'License URI: <%= pkg.licenseURI %>',
-  'Text Domain: <%= pkg.textDomain %>',
-  'Tags: education, theme-options',
-  '',
-  '@version v<%= pkg.version %>',
-  '@author <%= pkg.authorHuman %>',
-  '',
-  '*/',
-  ''].join( '\n' );
+const banner = [ "/*",
+	"Theme Name: <%= pkg.themeName %>",
+	"Theme URI: <%= pkg.homepage %>",
+	"Author: <%= pkg.author %>",
+	"Author URI: <%= pkg.authorURI %>",
+	"Description: <%= pkg.description %>",
+	"Template: <%= pkg.template %>",
+	"Version: <%= pkg.version %>",
+	"License: <%= pkg.license %>",
+	"License URI: <%= pkg.licenseURI %>",
+	"Text Domain: <%= pkg.textDomain %>",
+	"Tags: education, theme-options",
+	"",
+	"@version v<%= pkg.version %>",
+	"@author <%= pkg.authorHuman %>",
+	"",
+	"*/",
+	"" ].join( "\n" );
 
 // Include plugins.
 
-var eslint = require( 'gulp-eslint' );
-var changed = require( 'gulp-changed' );
-var imagemin = require( 'gulp-imagemin' );
-var concat = require( 'gulp-concat' );
-var stripDebug = require( 'gulp-strip-debug' );
-var terser = require( 'gulp-terser' );
-var sass = require( 'gulp-sass' );
-var sourcemaps = require( 'gulp-sourcemaps' );
-var autoprefixer = require( 'autoprefixer' );
-var postcss = require( 'gulp-postcss' );
-var rename = require( 'gulp-rename' );
-var path = require( 'path' );
-var header = require( 'gulp-header' );
-var shell = require( 'gulp-shell' );
-
+const eslint = require( "gulp-eslint" );
+const changed = require( "gulp-changed" );
+const imagemin = require( "gulp-imagemin" );
+const concat = require( "gulp-concat" );
+const terser = require( "gulp-terser" );
+const sass = require( "gulp-sass" );
+const sourcemaps = require( "gulp-sourcemaps" );
+const autoprefixer = require( "autoprefixer" );
+const postcss = require( "gulp-postcss" );
+const header = require( "gulp-header" );
+const shell = require( "gulp-shell" );
 
 // Options.
-var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'compressed' //expanded, nested, compact, compressed
+const sassOptions = {
+	errLogToConsole: true,
+	outputStyle: "compressed", //expanded, nested, compact, compressed
 };
 
 // JS concat, strip debugging and minify
-gulp.task('scripts', scripts);
+gulp.task( "scripts", scripts );
 
-function scripts(done) {
-  gulp.src('./src/js/*.js')
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+function scripts( done ) {
+	gulp.src( "./src/js/*.js" )
+		.pipe( eslint() )
+		.pipe( eslint.format() )
+		.pipe( eslint.failAfterError() );
 
-  gulp.src('./src/js/*.js')
-    .pipe(concat('script.min.js'))
-    //.pipe(stripDebug())
-    .pipe(terser())
-    .pipe(header(banner, { pkg : pkg } ))
-    .pipe(gulp.dest('./js/'));
-    
+	gulp.src( "./src/js/*.js" )
+		.pipe( concat( "script.min.js" ) )
+		.pipe( terser() )
+		.pipe( header( banner, { pkg } ) )
+		.pipe( gulp.dest( "./js/" ) );
+
 	done();
- // console.log('scripts ran');
+	// console.log('scripts ran');
 }
 
 // Theme CSS concat, auto-prefix and minify
-gulp.task('styles', styles);
+gulp.task( "styles", styles );
 
-function styles(done) {
+function styles( done ) {
+	gulp.src( "./src/sass/*.scss" )
+		.pipe( sourcemaps.init() )
+		.pipe( sass( sassOptions ).on( "error", sass.logError ) )
+		.pipe( concat( "style.css" ) )
+		.pipe( postcss( [ autoprefixer() ] ) )
+		.pipe( header( banner, { pkg } ) )
+		.pipe( sourcemaps.write( "./map" ) )
+		.pipe( gulp.dest( "." ) );
 
-	gulp.src('./src/sass/*.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass(sassOptions).on('error', sass.logError))
-		.pipe(concat('style.css'))
-        .pipe(postcss([ autoprefixer() ]))
-		.pipe(header(banner, { pkg : pkg } ))
-		.pipe(sourcemaps.write('./map'))
-		.pipe(gulp.dest('.'));
-
-  done();
-  //console.log('styles ran');
+	done();
+	//console.log('styles ran');
 }
 
 // minify new images
-gulp.task('images', images);
+gulp.task( "images", images );
 
-function images(done) {
-  var imgSrc = './src/images/**/*',
-      imgDst = './images';
+function images( done ) {
+	const imgSrc = "./src/images/**/*",
+		imgDst = "./images";
 
-  gulp.src(imgSrc)
-    .pipe(changed(imgDst))
-    .pipe(imagemin())
-    .pipe(gulp.dest(imgDst));
+	gulp.src( imgSrc )
+		.pipe( changed( imgDst ) )
+		.pipe( imagemin() )
+		.pipe( gulp.dest( imgDst ) );
 	done();
-  //console.log('images ran');
+	//console.log('images ran');
 }
 
 // run codesniffer
-gulp.task('sniffs', sniffs);
+gulp.task( "sniffs", sniffs );
 
-function sniffs(done) {
-    
-    return gulp.src('.', {read:false})
-        .pipe(shell(['./.sniff']));
-
+function sniffs( done ) {
+	return gulp.src( ".", { read: false } )
+		.pipe( shell( [ "./.sniff" ] ) );
 }
 
 // watch
-gulp.task('watcher', watcher);
+gulp.task( "watcher", watcher );
 
-function watcher(done) {
+function watcher( done ) {
 	// watch for JS changes
-	gulp.watch('./src/js/*.js', scripts);
+	gulp.watch( "./src/js/*.js", scripts );
 
 	// watch for Theme CSS changes
-	gulp.watch('./src/sass/**/*', styles);
-	
+	gulp.watch( "./src/sass/**/*", styles );
+
 	// watch for image changes
-	gulp.watch('./src/images/**/*', images);
-    
-    // watch for PHP change
-    gulp.watch('./**/*.php', sniffs);
+	gulp.watch( "./src/images/**/*", images );
+
+	// watch for PHP change
+	gulp.watch( "./**/*.php", sniffs );
 
 	done();
 }
 
-gulp.task( 'default',
-	gulp.parallel('images', 'scripts', 'styles', 'sniffs', 'watcher', function(done){
+gulp.task( "default",
+	gulp.parallel( "images", "scripts", "styles", "sniffs", "watcher", function( done ) {
 		done();
-	})
+	} )
 );
 
-
 function done() {
-	console.log('done');
+	console.log( "done" );
 }
