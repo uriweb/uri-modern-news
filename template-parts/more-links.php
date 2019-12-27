31f5ade7bb0b3f677d976939913363383c09857d
John Pennypacker <jpennypacker@uri.edu>
2019-12-27 12:17:04 -0500

    removes unused ACF fields; drops Foundation link from next story promo

--- template-parts/more-links.php
+++ template-parts/more-links.php
@@ -1,8 +1,4 @@
 <?php
-	$more_text = uri_modern_news_get_field( 'more_text', $post->ID );
-	if ( empty( $more_text ) ) {
-		$more_text = '<a href="https://securelb.imodules.com/s/1638/03-Foundation/interior-hybrid.aspx?sid=1638&gid=3&pgid=770&cid=2270&utm_source=news&utm_medium=more&utm_campaign=giving">Support the University of Rhode Island</a>.';
-	}
 
 	$previous_image = '';
 	$previous_post = get_previous_post( true );
@@ -11,7 +7,6 @@
 		$previous_image = str_replace( 'style="width:150px" class="', 'class="', $previous_image );
 	}
 
-
 ?>
 <div class="end-of-article-call" id="uri-next">
 	<h4>Next:</h4>
@@ -19,8 +14,7 @@
 	<?php
 		previous_post_link( '%link', $previous_image . '%title', true );
 	?>
-	</div><br />
-	<?php print nl2br( trim( $more_text ) ); ?>
+	</div>
 	
 </div>
 
