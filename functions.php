5421ec934d964f8417a358709549c62c8bf63381
Brandon Fuller <bjcfuller@uri.edu>
2023-01-17 15:18:19 -0500

    Refactor display posts output

--- functions.php
+++ functions.php
@@ -63,10 +63,9 @@ function uri_modern_news_get_field( $field_name, $post_id, $single = false ) {
  */
 function uri_modern_news_output_filter( $output, $original_atts, $image, $title, $date, $excerpt, $inner_wrapper, $content, $class, $author, $category_display_text ) {
 
+	$id = get_the_ID();
 	if ( $post ) {
 		$id = $post->ID;
-	} else {
-		$id = get_the_ID();
 	}
 
 	// Only show a few things for Media Mention posts
@@ -95,100 +94,51 @@ function uri_modern_news_output_filter( $output, $original_atts, $image, $title,
 	// but we're just going to hard-code it here for now
 	if ( ! empty( $original_atts['news_component'] ) ) {
 
-		if ( 'default' == $original_atts['news_component'] ) {
+		$horizontal_img_src = uri_modern_news_get_display_image( 'horizontal', $id );
+		$vertical_img_src = uri_modern_news_get_display_image( 'vertical', $id );
+		$post_headline = uri_modern_news_get_display_headline( $id );
 
-			$homepage_format = uri_modern_news_get_field( 'homepage_format', $id, false );
+		// Default
+		if ( 'default' == $original_atts['news_component'] ) {
 
 			// Set a default
 			if ( ! empty( $original_atts['default_component'] ) ) {
 				$original_atts['news_component'] = $original_atts['default_component'];
 			}
 
+			// Otherwise, use what's specified in the post
+			$homepage_format = uri_modern_news_get_field( 'homepage_format', $id, false );
 			if ( ! empty( $homepage_format ) ) {
 				$original_atts['news_component'] = $homepage_format;
 			}
+}
+
+		// Cards
+		if ( 'card' == $original_atts['news_component'] && function_exists( 'uri_cl_shortcode_card' ) ) {
+			$sc = '[cl-card title="' . uri_modern_news_escape_brackets( $post_headline ) . '" body="' . uri_modern_news_escape_brackets( get_the_excerpt() ) . '" link="' . get_the_permalink() . '" img="' . $horizontal_img_src . '" button="Read More"]';
+			$output = do_shortcode( $sc );
 		}
 
-		if ( 'card' == $original_atts['news_component'] ) {
+		// Panels
+		if ( 'panel' == $original_atts['news_component'] && function_exists( 'uri_cl_shortcode_panel' ) ) {
+			$sc = '[cl-panel format="super" title="' . uri_modern_news_escape_brackets( $post_headline ) . '" img="' . $vertical_img_src . '"]';
+			$sc .= uri_modern_news_escape_brackets( get_the_excerpt() );
 
-			$alternate_image = uri_modern_news_get_field( 'horizontal_image', $id, false );
-			$img_src = get_the_post_thumbnail_url( null, $original_atts['image_size'] );
-
-			// If there's a horizontal image set, prefer that
-			if ( ! empty( $alternate_image ) ) {
-				$img_src = wp_get_attachment_image_src( $alternate_image, 'original' )[0];
+			$panel_link = '<a href="' . get_the_permalink() . '">Read More</a>';
+			if ( function_exists( 'uri_cl_shortcode_button' ) ) {
+				$panel_link = '[cl-button link="' . get_the_permalink() . '" text="Read More"]';
 			}
 
-			$short_headline = uri_modern_news_get_field( 'short_headline', $id, false );
-			$post_headline = get_the_title();
+			$sc .= '<p>' . $panel_link . '</p>';
+			$sc .= '[/cl-panel]';
 
-			// If there's an alternate headline, prefer that
-			if ( ! empty( $short_headline ) ) {
-				$post_headline = $short_headline;
-			}
-
-			if ( function_exists( 'uri_cl_shortcode_card' ) ) {
-				$sc = '[cl-card title="' . uri_modern_news_escape_brackets( $post_headline ) . '" body="' . uri_modern_news_escape_brackets( get_the_excerpt() ) . '" link="' . get_the_permalink() . '" img="' . $img_src . '" button="Read More"]';
-				$output = do_shortcode( $sc );
-			}
+			$output = do_shortcode( $sc );
 		}
 
-		if ( 'panel' == $original_atts['news_component'] ) {
-
-			$alternate_image = uri_modern_news_get_field( 'vertical_image', $id, false );
-			$img_src = get_the_post_thumbnail_url( null, $original_atts['image_size'] );
-
-			// If there's a vertical image set, prefer that
-			if ( ! empty( $alternate_image ) ) {
-				$img_src = wp_get_attachment_image_src( $alternate_image, 'original' )[0];
-			}
-
-			$short_headline = uri_modern_news_get_field( 'short_headline', $id, false );
-			$post_headline = get_the_title();
-
-			// If there's an alternate headline, prefer that
-			if ( ! empty( $short_headline ) ) {
-				$post_headline = $short_headline;
-			}
-
-			if ( function_exists( 'uri_cl_shortcode_panel' ) ) {
-				$sc = '[cl-panel format="super" title="' . uri_modern_news_escape_brackets( $post_headline ) . '" img="' . $img_src . '"]';
-				$sc .= uri_modern_news_escape_brackets( get_the_excerpt() );
-
-				$panel_link = '<a href="' . get_the_permalink() . '">Read More</a>';
-				if ( function_exists( 'uri_cl_shortcode_button' ) ) {
-					$panel_link = '[cl-button link="' . get_the_permalink() . '" text="Read More"]';
-				}
-
-				$sc .= '<p>' . $panel_link . '</p>';
-				$sc .= '[/cl-panel]';
-
-				$output = do_shortcode( $sc );
-			}
-		}
-
-		if ( 'hero' == $original_atts['news_component'] ) {
-
-			$alternate_image = uri_modern_news_get_field( 'horizontal_image', $id, false );
-			$img_src = get_the_post_thumbnail_url( null, $original_atts['image_size'] );
-
-			// If there's a horizontal image set, prefer that
-			if ( ! empty( $alternate_image ) ) {
-				$img_src = wp_get_attachment_image_src( $alternate_image, 'original' )[0];
-			}
-
-			$short_headline = uri_modern_news_get_field( 'short_headline', $id, false );
-			$post_headline = get_the_title();
-
-			// If there's an alternate headline, prefer that
-			if ( ! empty( $short_headline ) ) {
-				$post_headline = $short_headline;
-			}
-
-			if ( function_exists( 'uri_cl_shortcode_hero' ) ) {
-				$sc = '[cl-hero format="super" headline="' . uri_modern_news_escape_brackets( $post_headline ) . '" subhead="' . uri_modern_news_escape_brackets( get_the_excerpt() ) . '" link="' . get_the_permalink() . '" img="' . $img_src . '" button="Read More"]';
-				$output = do_shortcode( $sc );
-			}
+		// Heroes
+		if ( 'hero' == $original_atts['news_component'] && function_exists( 'uri_cl_shortcode_hero' ) ) {
+			$sc = '[cl-hero format="super" headline="' . uri_modern_news_escape_brackets( $post_headline ) . '" subhead="' . uri_modern_news_escape_brackets( get_the_excerpt() ) . '" link="' . get_the_permalink() . '" img="' . $horizontal_img_src . '" button="Read More"]';
+			$output = do_shortcode( $sc );
 		}
 	}
 
@@ -197,6 +147,32 @@ function uri_modern_news_output_filter( $output, $original_atts, $image, $title,
 }
 add_filter( 'display_posts_shortcode_output', 'uri_modern_news_output_filter', 10, 11 );
 
+/**
+ * Get the specified alternate featured image, if it exists
+ */
+function uri_modern_news_get_display_image( $format, $id ) {
+	$alternate_image = uri_modern_news_get_field( $format . '_image', $id, false );
+	$img_src = get_the_post_thumbnail_url( null, $original_atts['image_size'] );
+
+	if ( ! empty( $alternate_image ) ) {
+		$img_src = wp_get_attachment_image_src( $alternate_image, 'original' )[0];
+	}
+	return $img_src;
+}
+
+/**
+ * Get the alternate headline, if it exists
+ */
+function uri_modern_news_get_display_headline( $id ) {
+	$short_headline = uri_modern_news_get_field( 'short_headline', $id, false );
+	$post_headline = get_the_title();
+
+	// If there's an alternate headline, prefer that
+	if ( ! empty( $short_headline ) ) {
+		$post_headline = $short_headline;
+	}
+	return $post_headline;
+}
 
 /**
  * Helper function to excape square brackets in text that would otherwise appear in shortcodes
@@ -210,6 +186,9 @@ function uri_modern_news_escape_brackets( $input ) {
 
 /**
  * Display only sticky posts
+ *
+ * Keeping this here for legacy compatibility, since
+ * we're going to move to categories instead of sticky posts -- BF
  *
  * @see https://displayposts.com/2019/01/09/display-or-hide-sticky-posts/
  */
